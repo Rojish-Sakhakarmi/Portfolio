ad27f821a2229fc46dc08c27cdcf90019bc62e25
Rojish-Sakhakarmi <rojish180@gmail.com>
2026-01-11 23:10:42 +0545

    add speedinsights

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights"; // <-- import SpeedInsights
+import { SpeedInsights } from "@vercel/speed-insights/next" // <-- import SpeedInsights
 
 const queryClient = new QueryClient();
 
